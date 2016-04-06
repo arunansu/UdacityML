4156d4710e40bc7f2f4134fcca9df8b18c88d6ec
arunansu <arunansu@hotmail.com>
2016-04-06 14:34:54 -0400

    added K-Means with 3 cluster

--- customer_segments/customer_segments.py
+++ customer_segments/customer_segments.py
@@ -31,11 +31,11 @@ print data.head()  # print the first 5 rows
 
 # **1)** In this section you will be using PCA and ICA to start to understand the structure of the data. Before doing any computations, what do you think will show up in your computations? List one or two ideas for what might show up as the first PCA dimensions, or what type of vectors will show up as ICA dimensions.
 
-# Answer: First PCA dimension would be the line connecting points that have features close to mean. ICA dimensions will be the lines connecting modes.
+# Answer: First PCA dimension would be the feature with maximum variance. ICA dimensions will be independent vectors without any correlation with each other.
 
 # ###PCA
 
-# In[3]:
+# In[2]:
 
 # TODO: Apply PCA with the same number of dimensions as variables in the dataset
 from sklearn.decomposition import PCA
@@ -53,11 +53,11 @@ print pca.explained_variance_ratio_
 
 # **3)** What do the dimensions seem to represent? How can you use this information?
 
-# Answer: The dimensions represent composite features that we can use instead of all the features in the dataset to reduce list of features and still be able to use it for prediction.
+# Answer: The first PCA component seems to coorelate with Frozen and the second PCA component seems to correlate with Detergents_Paper. The two major PCA components can be used to group customers more easily by observing their spending habits in these two categories.
 
 # ###ICA
 
-# In[4]:
+# In[3]:
 
 # TODO: Fit an ICA model to the data
 # Note: Adjust the data to have center at the origin first!
@@ -71,7 +71,7 @@ print ica.components_
 
 # **4)** For each vector in the ICA decomposition, write a sentence or two explaining what sort of object or property it corresponds to. What could these components be used for?
 
-# Answer: These components represent a matrix that can be used to transform the given data projected on independent axis. 
+# Answer: Each vector in ICA decomposition represents a type of customer. The first vector represents a customer that mostly buys fresh, and then detergents, delicatessen, milk, possibly a farmer's market store shopper. The second vector represents a customer that mostly buys fresh and detergents, possibly shopping at a small grocery store. The third vector represents a shopper who mostly buys frozen and then deli and grocery, possibly a deli/grocery store shopper. The fourth vector represents a customer that mostly buys milk, possibly a convinience store shopper. The fifth vector is a shopper that mostly buys frozen, grocery and some fresh and milk, possibly a chain grocery store shopper. The sixth vector represents a shopper that mostly buys deli and some fresh and detergents, possibly a small deli/grocery store shopper. These components can be used to group shoppers into different categories.
 
 # ##Clustering
 # 
@@ -81,18 +81,18 @@ print ica.components_
 # 
 # **5)** What are the advantages of using K Means clustering or Gaussian Mixture Models?
 
-# Answer: In K Means clustering we can pick the number of clusters we want and the algorithm will create exactly that many clusters. So each feature is assigned to exactly one cluster. But in Gaussian Mixture Model, each feature is assigned to one or many clusters with a certain probility. K Means clustering could be thought of as special case of Gaussian Mixed Model clustering, where the probability of a feature being in K clusters is 1 for a particular cluster and 0 for others.
+# Answer: In K-Means clustering we can pick the number of clusters we want and the algorithm will create exactly that many clusters. So each feature is assigned to exactly one cluster. But in Gaussian Mixture Model, each feature is assigned to one or many clusters with a certain probility. K Means clustering could be thought of as special case of Gaussian Mixed Model clustering, where the probability of a feature being in K clusters is 1 for a particular cluster and 0 for others. GMM is more complex and tends to be slower than K-Means algorithm, because it maintains partial membership information for each data point and it needs to update the estimate for covariance matrix on each iteration. GMM generally tends to be more accurate compared to K-Means algorithm except when K is very high and the feature distribution is non-gaussian. Both K-Means and GMM work well with large number of samples, producing many clusters. K-Meas is useful when smaller number of clusters with even cluster size is preferred, where as GMM is preferred when density estimation is important. In the current dataset since there are two major PCA dimensions, we can expect a good cluster distribution for 2 clusters and since shoppers are chosen at random, gaussian distribution can be assumed. This makes the dataset ideal for K-Means clustering.
 
 # **6)** Below is some starter code to help you visualize some cluster data. The visualization is based on [this demo](http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html) from the sklearn documentation.
 
-# In[5]:
+# In[4]:
 
 # Import clustering modules
 from sklearn.cluster import KMeans
 from sklearn.mixture import GMM
 
 
-# In[14]:
+# In[31]:
 
 # TODO: First we reduce the data to two dimensions using PCA to capture variation
 pca = PCA(n_components=2)
@@ -100,17 +100,20 @@ reduced_data = pca.fit_transform(data)
 print reduced_data[:10]  # print upto 10 elements
 
 
-# In[16]:
+# In[32]:
 
 # TODO: Implement your clustering algorithm here, and fit it to the reduced data for visualization
 # The visualizer below assumes your clustering object is named 'clusters'
 
-clusters = KMeans(init='k-means++', n_clusters=2, n_init=10)
-clusters.fit(reduced_data)
-print clusters
+clusters2 = KMeans(init='k-means++', n_clusters=2, n_init=10)
+clusters3 = KMeans(init='k-means++', n_clusters=3, n_init=10)
+clusters2.fit(reduced_data)
+clusters3.fit(reduced_data)
+print clusters2
+print clusters3
 
 
-# In[17]:
+# In[27]:
 
 # Plot the decision boundary by building a mesh grid to populate a graph.
 x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
@@ -120,23 +123,32 @@ hy = (y_max-y_min)/1000.
 xx, yy = np.meshgrid(np.arange(x_min, x_max, hx), np.arange(y_min, y_max, hy))
 
 # Obtain labels for each point in mesh. Use last trained model.
-Z = clusters.predict(np.c_[xx.ravel(), yy.ravel()])
+Z2 = clusters2.predict(np.c_[xx.ravel(), yy.ravel()])
+Z3 = clusters3.predict(np.c_[xx.ravel(), yy.ravel()])
 
 
-# In[25]:
+# In[33]:
 
 # TODO: Find the centroids for KMeans or the cluster means for GMM 
-centroids = clusters.cluster_centers_
-print centroids
+centroids2 = clusters2.cluster_centers_
+centroids3 = clusters3.cluster_centers_
+print "Centeroids for K-Means with 2 clusters"
+print centroids2
+print "Inverse Transform for K-Means with 2 clusters"
+print pca.inverse_transform(centroids2)
+print "Centeroids for K-Means with 3 clusters"
+print centroids3
+print "Inverse Transform for K-Means with 3 clusters"
+print pca.inverse_transform(centroids3)
 
 
-# In[26]:
+# In[29]:
 
 # Put the result into a color plot
-Z = Z.reshape(xx.shape)
+Z2 = Z2.reshape(xx.shape)
 plt.figure(1)
 plt.clf()
-plt.imshow(Z, interpolation='nearest',
+plt.imshow(Z2, interpolation='nearest',
            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
            cmap=plt.cm.Paired,
            aspect='auto', origin='lower')
@@ -153,21 +165,41 @@ plt.xticks(())
 plt.yticks(())
 plt.show()
 
+Z3 = Z3.reshape(xx.shape)
+plt.figure(2)
+plt.clf()
+plt.imshow(Z3, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+plt.scatter(centroids3[:, 0], centroids3[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.title('Clustering on the wholesale grocery dataset (PCA-reduced data)\n'
+          'Centroids are marked with white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
 
 # **7)** What are the central objects in each cluster? Describe them as customers.
 
-# Answer: The central objects in each cluster represent a type of customer.
+# Answer: The central objects in each cluster represent a type of customer. For example in K-Means clustering with 2 clusters, the left cluster represents customers who shop at farmers market stores and the right cluster represents customers who shop at chain grocery stores. In case of K-Means clustering with 3 clusters the left bottom cluster represents customers who shop at farmer's market stores. The top cluster represents customers who shop at deli grocery stores and the bottom right cluster represents customes who shop at chain grocery stores.
 
 # ###Conclusions
 # 
 # ** 8)** Which of these techniques did you feel gave you the most insight into the data?
 
-# Answer: PCA was helpful in identifying the transformed features that are useful to do clustering.
+# Answer: K-Means clustering helped divide the customers into 2 major groups. 
 
 # **9)** How would you use that technique to help the company design new experiments?
 
-# Answer: I would use clustering to create groups of items and people that are similar.
+# Answer: Since K-Means clustering helped identify the 2 major types of customers, I would use this information to design experiments for the company where 2 different stores could be arranged differently, with one having fresh, milk and deli at the front and frozen, detergents and grocery at the back and the other having frozen, detergents and grocery at the front and fresh, milk and deli at the back. There can also be other stores with a different arrangement of items. The results of the experiment would help suggest the most optimal way to arrange items at stores to generate most revenue. Also this will help decide what type of items should be sold more to what type of stores.
 
 # **10)** How would you use that data to help you predict future customer needs?
 
-# Answer: I would use the data to decide on what to stock in the store and what type of customers to cater to.
+# Answer: Knowing different types of customers and their spending habbits, I would use linear regression to predict their future demand for specific types of items.
